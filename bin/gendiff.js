1eb228e2e2277ad941f26a5bec14d943b335da6c
Vain560 <solodov.82@yandex.ru>
2024-07-16 12:31:57 +0500

    Adjusting argumentsand options for gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,4 +7,14 @@ const program = new Command();
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv);
+
+  const options = program.opts();
+  const [filepath1, filepath2] = program.args;
+
+  console.log('Filepath1', filepath1);
+  console.log('Filepath2', filepath2);
+  console.log('Format:', options.format);
